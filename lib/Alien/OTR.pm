8f8456a01701c1bf066ee1761a18d541aa753c93
Alex J. G. Burzyński <ajgb@cpan.org>
2014-02-04 00:25:37 +0000

    Import of AJGB/Alien-OTR-4.0.0.0 from CPAN.
    gitpan-cpan-distribution: Alien-OTR
    gitpan-cpan-version:      4.0.0.0
    gitpan-cpan-path:         AJGB/Alien-OTR-4.0.0.0.tar.gz
    gitpan-cpan-author:       AJGB
    gitpan-cpan-maturity:     released

---
+++ lib/Alien/OTR.pm
@@ -0,0 +1,58 @@
+# ABSTRACT: Install and make available libotr v4 library
+
+use strict;
+use warnings;
+package Alien::OTR;
+BEGIN {
+  $Alien::OTR::AUTHORITY = 'cpan:AJGB';
+}
+$Alien::OTR::VERSION = '4.0.0.0';
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Alien::OTR - Install and make available libotr v4 library
+
+=head1 VERSION
+
+version 4.0.0.0
+
+=head1 SYNOPSIS
+
+    use Alien::OTR;
+
+    my $cflags = Alien::OTR->cflags;
+    my $libs = Alien::OTR->libs;
+
+=head1 DESCRIPTION
+
+Alien::OTR installs the C library C<libotr> version v4.0.0.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<https://otr.cypherpunks.ca/>
+
+=back
+
+=head1 AUTHOR
+
+Alex J. G. Burzyński <ajgb@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Alex J. G. Burzyński <ajgb@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
